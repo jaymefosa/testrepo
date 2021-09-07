68657b06513683b3c1033f75c4a4e5f27bb631fa
Jayme <jayme.c.fosa@gmail.com>
2021-09-07 14:37:54 +0900

    hm

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from fontSetup import setFont2
---
+++ fontSetup.py
@@ -0,0 +1,25 @@
+def setFont():
+    css_str = """
+        <link rel="stylesheet"
+              href="https://fonts.googleapis.com/css?family=Roboto|Urbana|Tangerine|Roboto Mono&effect=shadow-multiple|
+              fire-animation|neon|outline">
+        <style>
+          div.roboto {
+            font-family: 'Roboto';
+            font-size: 28px;
+          }
+        div.tangerine {
+            font-family: 'Tangerine';
+            font-size: 38px;
+          }
+        div.fakeh1tan {
+            font-family: 'Tangerine';
+            font-size: 68px;
+          }
+        </style>
+        
+        
+        
+
+    """
+    return css_str
